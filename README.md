c329d0ce8b1443f6dd6f48d338bbfa095e53a704
Elijah Scheele <eli@zany.tech>
2020-07-30 14:33:12 -0700

    prelim chart working

--- README.md
+++ README.md
@@ -6,6 +6,6 @@ The goals of this project are as follows:
 
 * Use the experimental WebBluetooth API to connect to, read, and display measurements from the HEGDuino device.
 * Learn more about the [Svelte](https://svelte.dev) web app framework, including unit testing using jest
-* Learn more about the [MetricsGraphics.js](https://github.com/metricsgraphics/metrics-graphics) library to display the data collected by the web application
+* Learn more about the [Taucharts](https://taucharts.com/) library to display the data collected by the web application
 
 Eventually, this front-end will be paired with a robust backend to associate measurements with users, adding things like authentication, authorization and persistent storage of readings.
--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,55 @@
+<svelte:head>
+  <script src="https://cdn.jsdelivr.net/npm/d3@4.13.0/build/d3.min.js" charset="utf-8"></script>
+  <script src="https://cdn.jsdelivr.net/npm/taucharts@2/dist/taucharts.min.js" type="text/javascript"></script>
+</svelte:head>
 <script>
- // import "../node_modules/bulma/css/bulma.min.css"
+  import "../node_modules/bulma/css/bulma.min.css"
+  import "../node_modules/taucharts/dist/taucharts.min.css"
   import BluetoothManager from "./BluetoothManager.svelte"
+
+  let dataArray = [];
   function handleNewValue(event) {
-    console.log('New value: ', event.detail.text)
+    parseValue(event.detail.text).forEach( dataPoint => {
+      dataArray.push(dataPoint);
+    })
+
+    if (dataArray.length > 1000) {
+      drawChart(dataArray);
+      dataArray = [];
+    }
+
+
+  }
+
+  function parseValue(str) {
+    let valArray = str.split("");
+
+    return [{
+      time: parseInt(valArray[0]),
+      reading: parseFloat(valArray[1]),
+      type: 'red'
+    }, {
+      time: parseInt(valArray[0]),
+      reading: parseFloat(valArray[1]),
+      type: 'ir'
+    }]
+  }
+  
+  function drawChart(dataArray){
+    let chart = new Taucharts.Chart({
+      data: dataArray,
+      type: 'line',
+      x: 'time',
+      y: 'reading',
+      color: 'type'
+    });
+
+    chart.renderTo('#chart');
   }
 </script>
 
 <main>
   <h1>Welcome to the HEG Monitor!</h1>
-  <BluetoothManager on:value={handleNewValue}/>
+  <BluetoothManager on:value={handleNewValue}/> 
+  <div id="chart">
 </main>

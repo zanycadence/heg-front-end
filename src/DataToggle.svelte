dc368bcbd781772df15a7ed93665ef8e0b4bba71
Elijah Scheele <eli@zany.tech>
2020-07-30 13:56:39 -0700

    basic bluetooth connection establishment complete, added toggle button for sensor readings - good spot to work on data passing to visualization library

---
+++ src/DataToggle.svelte
@@ -0,0 +1,20 @@
+<script>
+  import { createEventDispatcher } from 'svelte';
+
+  export let dataActive = false;
+
+  const dispatch = createEventDispatcher();
+
+  function toggleData(){
+    dataActive = !dataActive;
+    dispatch('toggle', {
+      isActive: dataActive
+    })
+  }
+</script>
+
+{#if dataActive}
+  <button on:click={toggleData}>Stop Measurement</button>
+{:else}
+<button on:click={toggleData}>Start Measurement</button>
+{/if}

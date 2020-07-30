d6773c27e3b876aad67138cdd39ea9c746e528d2
Elijah Scheele <eli@zany.tech>
2020-07-30 13:57:36 -0700

    removed unnecessary style

--- src/App.svelte
+++ src/App.svelte
@@ -10,25 +10,3 @@
   <h1>Welcome to the HEG Monitor!</h1>
   <BluetoothManager on:value={handleNewValue}/>
 </main>
-
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
